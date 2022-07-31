907c64c69c303edfd185174dc66ebf3abb1dae5b
Camila-TS <camilathompson@gmail.com>
2022-07-31 17:15:29 -0400

    creamos json con las tarjetas del juego

---
+++ src/data/cartoon-network/cartoon-network.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'Pokemon',
+  items: [
+    { id: 'adventureTime', image: 'https://i.ibb.co/jwGcCJv/1.jpg', /*bgColor: '#339933'*/ },
+    { id: 'powerpuffGirls', image: 'https://i.ibb.co/tL3pMsR/2.png', /*bgColor: '#339933'*/ },
+    { id: 'dextersLab', image: 'https://i.ibb.co/qxB4Mkz/3.png', /*bgColor: '#339933'*/ },
+    { id: 'courage', image: 'https://i.ibb.co/7bRzDDt/4.png',/* bgColor: '#F05032' */},
+    { id: 'scoobyDoo', image: 'https://i.ibb.co/KzknnwX/5.png', /*bgColor: '#F05032'*/ },
+    { id: 'johnnyBravo', image: 'https://i.ibb.co/XsfqdM8/bravo.png', /*bgColor: '#F05032'*/ }
+   /* { id: 'squirtle', image: 'https://www.serebii.net/pokemongo/pokemon/007.png', bgColor: '#1572B6' },
+    { id: 'wartortle', image: 'https://www.serebii.net/pokemongo/pokemon/008.png', bgColor: '#1572B6' },
+    { id: 'blastoise', image: 'https://www.serebii.net/pokemongo/pokemon/009.png', bgColor: '#1572B6' },*/
+  ],
+};
