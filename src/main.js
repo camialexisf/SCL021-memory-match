581d74a2c9b3c043db47d3889dea754959ae6da8
Camila-TS <camilathompson@gmail.com>
2022-08-08 19:17:25 -0400

    primer intento para crear funcion que voltee la carta clickeada, agregamos exitosamente imagenes al readme

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import App from './components/App.js';
-import shuffle from './data/cartoon-network/cartoon-network.js';
-
+//import data from './data/cartoon-network/cartoon-network.js';
+/*import flipCard from './components/flipcard.js';*/
 
 document.getElementById('root').appendChild(App());
 
-let arrCards = document.querySelectorAll(".frontcards")
-let backImg = shuffle.items[0].backSide;
 
-
-setTimeout(function(){
-    for(let i = 0;i < arrCards.length; i++){
-        arrCards[i].setAttribute("src",backImg )
-    }
-}, 5000)
