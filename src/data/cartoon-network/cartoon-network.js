0dceb2bc58eb3960feefd9d8840509979c8672f0
Camila-TS <camilathompson@gmail.com>
2022-08-09 16:54:23 -0400

    creamos función que permite destapar cartas en pares y las compara, arreglamos estilos en CSS

--- src/data/cartoon-network/cartoon-network.js
+++ src/data/cartoon-network/cartoon-network.js
@@ -1,14 +1,15 @@
 export default {
   name: 'cartoon-network',
+  backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png',
   items: [
-    { id: 'adventureTime', image: 'https://i.ibb.co/jwGcCJv/1.jpg', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png'},
-    { id: 'powerpuffGirls', image: 'https://i.ibb.co/tL3pMsR/2.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png' },
-    { id: 'dextersLab', image: 'https://i.ibb.co/qxB4Mkz/3.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png' },
-    { id: 'courage', image: 'https://i.ibb.co/7bRzDDt/4.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png'},
-    { id: 'scoobyDoo', image: 'https://i.ibb.co/KzknnwX/5.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png' },
-    { id: 'johnnyBravo', image: 'https://i.ibb.co/XsfqdM8/bravo.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png' },
-    /*{ id: 'squirtle', image: 'https://www.serebii.net/pokemongo/pokemon/007.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png'},
-    { id: 'wartortle', image: 'https://www.serebii.net/pokemongo/pokemon/008.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png'},
-    { id: 'blastoise', image: 'https://www.serebii.net/pokemongo/pokemon/009.png', backSide: 'https://i.ibb.co/KWydGw9/Sin-t-tulo-1.png'},*/
+    { id: 'adventureTime', image: 'https://i.ibb.co/jwGcCJv/1.jpg'},
+    { id: 'powerpuffGirls', image: 'https://i.ibb.co/tL3pMsR/2.png'},
+    { id: 'dextersLab', image: 'https://i.ibb.co/qxB4Mkz/3.png'},
+    { id: 'courage', image: 'https://i.ibb.co/7bRzDDt/4.png'},
+    { id: 'scoobyDoo', image: 'https://i.ibb.co/KzknnwX/5.png'},
+    { id: 'johnnyBravo', image: 'https://i.ibb.co/XsfqdM8/bravo.png'},
+    /*{ id: 'squirtle', image: 'https://www.serebii.net/pokemongo/pokemon/007.png'},
+    { id: 'wartortle', image: 'https://www.serebii.net/pokemongo/pokemon/008.png'},
+    { id: 'blastoise', image: 'https://www.serebii.net/pokemongo/pokemon/009.png'},*/
   ],
 };
